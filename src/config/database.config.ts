4753ac044ca1036971bf3bce98958ed0da0e8b22
Shivam Verma <shivam.v@weboniselab.com>
2025-09-12 17:03:43 +0530

    Neura Lens Server: Changed app_name key name in DB config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,9 +2,9 @@ import { registerAs } from '@nestjs/config';
 import { DatabaseConfig } from '../common/interfaces/database-config.interface';
 
 export default registerAs<DatabaseConfig>('database', (): DatabaseConfig => {
-  const {DB_USER, DB_PASS, DB_NAME, APP_NAME, DB_CLUSTER } = process.env;
+  const {DB_USER, DB_PASS, DB_NAME, DB_APP_NAME, DB_CLUSTER } = process.env;
   return {
-    uri: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority&appName=${APP_NAME}`,
+    uri: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority&appName=${DB_APP_NAME}`,
     name: process.env.DB_NAME!,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
